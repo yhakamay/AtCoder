1a2c76a285de7b3dc94b6dd91c2a1c360b7d84cf
Yusuke <yhakamaya1031@gmail.com>
2021-01-14 00:17:36 +0900

    feat: ABC185/B.c

--- ABC185/B.c
+++ ABC185/B.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int	main(void)
+int main(void)
 {
-	float	n;
-	int		m, t, a, b;
+	int n, m, t;
+	scanf("%d %d %d", &n, &m, &t);
+	int charge = n, pos = 0;
 
-	scanf("%f %d %d", &n, &m, &t);
-	n -= 1 * t;
-	printf("n: %f\n", n);
-	for (int i = 0; i < m; i++)
+	while (m--)
 	{
+		int a, b;
 		scanf("%d %d", &a, &b);
-		n += 4 * (b - a);
-		if (n <= 0)
-			break ;
-		printf("n: %f\n", n);
+		charge -= a - pos;
+		if (charge < 1)
+			break;
+		charge += b - a;
+		pos = b;
+		charge = (charge > n) ? n : charge;
 	}
-	printf("n: %f\n", n);
-	printf("%s", (n > 0) ? "Yes" : "No");
+	puts(charge > t - pos ? "Yes" : "No");
 	return 0;
 }
